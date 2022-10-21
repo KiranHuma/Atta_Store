5171d0a9c54717f82ab4138d0ba04a8296356e31
Aimal Huma <66478525+KiranHuma@users.noreply.github.com>
2022-10-21 11:21:39 +0500

    code_encode_barcode

--- Atta_Store/My Project/Application.Designer.vb
+++ Atta_Store/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Atta_Store.AddInventory
+            Me.MainForm = Global.Atta_Store.barcode_genFrm
         End Sub
     End Class
 End Namespace
---
+++ Atta_Store/barcode_genFrm.vb
@@ -0,0 +1,17 @@
+﻿
+Imports ZXing
+Public Class barcode_genFrm
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim barwriter As New BarcodeWriter
+        barwriter.Format = BarcodeFormat.CODE_128
+        PictureBox1.Image = barwriter.Write(TextBox1.Text)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim barreader As New BarcodeReader
+        Dim res = barreader.Decode(CType(PictureBox1.Image, Bitmap))
+        If res IsNot Nothing Then
+            TextBox2.Text = res.Text
+        End If
+    End Sub
+End Class
